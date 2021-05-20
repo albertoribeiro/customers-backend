473823a8548c6fca25f6e20a79ee12854981813c
albertoribeiro <alberto.r.flavio@gmail.com>
2021-05-20 18:35:29 -0300

    Fix GraphQL

---
+++ src/api/handler.js
@@ -0,0 +1,7 @@
+module.exports = {
+  customersCreate: require('./controllers/customers/create'),
+  customersReadAll: require('./controllers/customers/readAll'),
+  customersReadOne: require('./controllers/customers/readOne'),
+  customersUpdate: require('./controllers/customers/update'),
+  customersDelete: require('./controllers/customers/delete')
+}
--- src/handler.js
+++ src/handler.js
@@ -1,39 +1,36 @@
-// 'use strict';
-// const { ApolloServer, gql } = require('apollo-server-lambda');
-// const setupDynamoDBClient = require('./core/util/setupDynamoDB')
-// setupDynamoDBClient()
+'use strict';
+const { ApolloServer, gql } = require('apollo-server-lambda');
+const setupDynamoDBClient = require('./core/util/setupDynamoDB')
+setupDynamoDBClient()
 
-// const CustomerFactory = require('./core/factories/customerFactory')
-// const schema = require('./graphql')
-// const isLocal = process.env.IS_LOCAL
+const CustomerFactory = require('./core/factories/customerFactory')
+const schema = require('./graphql')
+const isLocal = process.env.IS_LOCAL
 
-// const server = new ApolloServer({
-//   schema,
-//   context: async() => ({
-//     Customer: await CustomerFactory.createInstance()
-//   }),
-//   introspection: isLocal,
-//   playground: isLocal,
-//   formatError(error) {
-//     console.error('[Global error logger]', error)
-//     return error
-//   },
-// });
+const server = new ApolloServer({
+  schema,
+  context: async() => ({
+    Customer: await CustomerFactory.createInstance()
+  }),
+  introspection: isLocal,
+  playground: isLocal,
+  formatError(error) {
+    console.error('[Global error logger]', error)
+    return error
+  },
+});
 
 
-// exports.graphQL =  server.createHandler({
-//   cors: {
-//     origin: '*', 
-//     allowedHeaders: ['Origin','X-Requested-With','Content-Type','Accept','content-type','application/json'],
-//     credentials: true,
-//     methods: ['GET','PUT','POST','DELETE','OPTIONS']
-//   },
-// });
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*', 
+  },
+});
 
-module.exports = {
-  customersCreate: require('./api/controllers/customers/create'),
-  customersReadAll: require('./api/controllers/customers/readAll'),
-  customersReadOne: require('./api/controllers/customers/readOne'),
-  customersUpdate: require('./api/controllers/customers/update'),
-  customersDelete: require('./api/controllers/customers/delete')
-}
+// module.exports = {
+//   customersCreate: require('./api/controllers/customers/create'),
+//   customersReadAll: require('./api/controllers/customers/readAll'),
+//   customersReadOne: require('./api/controllers/customers/readOne'),
+//   customersUpdate: require('./api/controllers/customers/update'),
+//   customersDelete: require('./api/controllers/customers/delete')
+// }
